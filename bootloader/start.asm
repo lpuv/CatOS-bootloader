b987e4ac2fe3b11617e70f3a9a7472e5313738be
ZareZare3 <39651113+ZareZare3@users.noreply.github.com>
2019-12-14 11:10:10 -0800

    change boot offset

--- bootloader/start.asm
+++ bootloader/start.asm
@@ -5,7 +5,7 @@ disk_load:
     ; to the stack for later use.
     push dx
 
-    mov ah, 0x02 ; ah <- int 0x13 function. 0x02 = 'read'
+    mov ah, 0x1000 ; ah <- int 0x13 function. 0x02 = 'read'
     mov al, dh   ; al <- number of sectors to read (0x01 .. 0x80)
     mov cl, 0x02 ; cl <- sector (0x01 .. 0x11)
                  ; 0x01 is our boot sector, 0x02 is the first 'available' sector
